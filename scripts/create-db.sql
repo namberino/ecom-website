c792fa265377c47efde7556eca870d50b76f031f
namberino <namnb2402@gmail.com>
2024-10-06 21:52:36 +0700

    Update db creation script

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -8,15 +8,3 @@ create table Users (
     password varchar(255) not null,
     primary key (id)
 );
-
-insert into Users (name, email, password) values
-("John Doe", "john@example.com", "password123"),
-("Jane Smith", "jane@example.com", "password123"),
-("Alice Johnson", "alice@example.com", "password123"),
-("Bob Williams", "bob@example.com", "password123"),
-("Charlie Brown", "charlie@example.com", "password123"),
-("David Harris", "david@example.com", "password123"),
-("Ella Davis", "ella@example.com", "password123"),
-("Frank Wilson", "frank@example.com", "password123"),
-("Grace Lee", "grace@example.com", "password123"),
-("Henry Adams", "henry@example.com", "password123");
