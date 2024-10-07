c4cf0031d86ec6fd7509ea9e5cb336eff6053327
namberino <namnb2402@gmail.com>
2024-10-07 09:06:53 +0700

    Remove unnecessary config

--- backend/app.py
+++ backend/app.py
@@ -10,12 +10,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-# mysql configs
-app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
-app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
-app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
-app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
-
 # connect to mysql
 db = MySQLdb.connect(
     host=os.getenv("MYSQL_HOST"),
