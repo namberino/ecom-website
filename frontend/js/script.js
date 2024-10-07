50c9485fdc9abe5a25fc0c9a10cd8924b64c1ecf
namberino <namnb2402@gmail.com>
2024-10-07 09:31:30 +0700

    Update missing field data message

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
                 }
             });
         } else {
-            alert("Missing data.");
+            alert("Missing data. Please fill in all fields.");
         }
     });
 
@@ -54,7 +54,7 @@ $(document).ready(function() {
                 }
             });
         } else {
-            alert("Please fill in all fields.");
+            alert("Missing data. Please fill in all fields.");
         }
     });
 });
