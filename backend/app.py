d2c3901fdff87554584bbdf1f1974bc7beb7c56e
namberino <namnb2402@gmail.com>
2024-10-09 21:50:45 +0700

    Update import files

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 import MySQLdb
 import bcrypt
-from dotenv import load_dotenv
 import os
 import rsa
 import base64
